a6a3a00bea5a592993886712a10c7b40b2608216
Bria White <bria.awhite@gmail.com>
2020-08-09 02:00:39 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,7 +28,7 @@ class Song
     song
   end
   
-  def self.find_by_name(title) #class finder
+  def self.find_by_name(title) 
     result = self.all.detect {|song| song.name == title}
     result
   end
